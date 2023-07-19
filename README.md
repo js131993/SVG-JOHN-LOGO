b70e2f489ef7b25315979e350ac8be12cde8f0c6
js131993 <john.sanders4444@gmail.com>
2023-07-19 16:41:42 -0400

    Fixing Questions
    Reformatting questions to fit scenario.  Will be producing an SVG file to produce the different svg shapes.

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # SVG-JOHN-LOGO
 
-package.json
-
-node modules
-
-inquirer
-
-jest
-
-CREATE A SHAPE TEST THEN 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // GIVEN a command-line application that accepts user input
 var inquirer = require('inquirer');
 const fs = require('fs');
-
 // WHEN I am prompted for text
 
 // THEN I can enter up to three characters
@@ -28,36 +27,34 @@ const fs = require('fs');
 
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
 // TODO: Include packages needed for this application
-var inquirer = require('inquirer');
-const fs = require('fs');
-
-
 // TODO: Create an array of questions for user input
 const questions = [{
-    name: "Q1",
-    message: "What is the project's title?"
-},
-{
-  name: "Q2",
-  message: "Give a description of the Project?",
-},
-{
-  name:"Q3",
-  type: "list",
-  message:"Choose a License",
-  choices: [
-    "Apache License 2.0",
-    "Artistic license",
-  ]
-},
-{
-  name: "Q4",
-  message: "What are the steps to install?"
-},
-{
-  name: "Q5",
-  message: "How do you use?",
-}
+            name: "Q1",
+            message: "Text to diplay inside of shape.",
+            validate: (answer) => answer.length < 4
+        },
+        {
+            name: "Q2",
+            message: "What would you like for the color of the text? (Example: #FF5733) ",
+        }, 
+        {
+            name:"Q3",
+            type: "list",
+            message:"Choose a shape",
+            choices: [
+                "Triangle",
+                "Circle",
+                "Square"
+        ]
+        },
+        {
+            name: "Q4",
+            message: "What would you like for the color of the shape? (Example: #FF5733) ",
+        },
+        {
+        name: "Q5",
+        message: "How do you use?",
+        }
 ];
 
 
@@ -100,11 +97,11 @@ function init() {
   inquirer.prompt(questions)
  
   .then((answers) => {
-    let readme = {};
-    readme.title = answers.Q1;
-    readme.description = answers.Q2;
-    readme.license = answers.Q3;
-    readme.installation = answers.Q4;
+    let svg = {};
+    svg.text = answers.Q1;
+    readme.textColor = answers.Q2;
+    readme.shape = answers.Q3;
+    readme.shapeColor = answers.Q4;
     readme.usage = answers.Q5;
     writeToFile("./READMETest.md", readme);
 
