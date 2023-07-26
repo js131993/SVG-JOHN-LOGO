1d2111909855f36071c1fd61c9b4a6af2971ff4d
js131993 <john.sanders4444@gmail.com>
2023-07-26 16:09:34 -0400

    Removing notes from
    Formatting server.js

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
-// GIVEN a command-line application that accepts user input
+
 var inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Shape, Triangle, Square } = require("./lib/shapes.js");
 const { validateHTMLColorName, validateHTMLColorHex } = require ("validate-color");
 
 var validateColor = require("validate-color").default;
-// WHEN I am prompted for text
 
-// THEN I can enter up to three characters
-
-// WHEN I am prompted for the text color
-
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I am prompted for a shape
-
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-
-// WHEN I am prompted for the shape's color
-
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I have entered input for all the prompts
-
-// THEN an SVG file is created named `logo.svg`
-
-// AND the output text "Generated logo.svg" is printed in the command line
-
-// WHEN I open the `logo.svg` file in a browser
-
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-// TODO: Include packages needed for this application
-// TODO: Create an array of questions for user input
 const questions = [
   {
     name: "Q1",
@@ -59,9 +33,6 @@ const questions = [
 ];
 
 // AN OBJECT IS A BUNDLE OF RELATED INFORMATION, MODEL THE REAL WORLD, Describing
-// TODO: Create a function to write README file
-
-// TODO: Create a function to initialize app
 function init() {
   inquirer
     .prompt(questions)
